e27ee72f60ea196b5d2287d7a4facc5d7a817551
Sascha Adler <contact@consonantwave.com>
2011-02-03 15:24:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fugue::Application.config.secret_token = '3838f0843d917064f3e0fb5d5b41aa71f82dc1e4cafc6b9ef44b7cc3d128b29c35152dcd037c0d38d4ca806884b7008e36d51094ec23e2a164740f00507bb452'
