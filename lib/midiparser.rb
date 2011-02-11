c7ab9566b42f5838e939211502c269c6fc440308
Sascha Adler <sascha.adler@gmail.com>
2011-02-11 00:06:15 -0500

    Improved midi parser's text support

--- lib/midiparser.rb
+++ lib/midiparser.rb
@@ -8,21 +8,25 @@ module Midiparser
     def parse
       @file.each_line do |line|
         data = line.chomp.split(", ")
-        case data[0]
-        when "0"
-          # parse header
-          if data[2] == "Header"
-            header(data[3].to_i, data[4].to_i, data[5].to_i)
-          end
-        when "1"
-          # parse metadata
-          metadata(data[2], data[3..-1])
-        else
-          # parse channel events
-          event(data[2], data[0].to_i, data[1].to_i, data[3..-1])
-        end
+        parse_line(data)
       end
       @piece.save
+    end
+
+    def parse_line(data)
+      case data[0]
+      when "0"
+        # parse header
+        if data[2] == "Header"
+          header(data[3].to_i, data[4].to_i, data[5].to_i)
+        end
+      when "1"
+        # parse metadata
+        metadata(data[2], data[3..-1])
+      else
+        # parse channel event
+        event(data[2], data[0].to_i, data[1].to_i, data[3..-1])
+      end
     end
 
     def header(format, num_tracks, division)
@@ -32,7 +36,7 @@ module Midiparser
     def metadata(type, args)
       case type
       when "Text_t"
-        @piece.title = args.reduce(:+)
+        @piece.title = handle_text(args) 
       when "Tempo"
         @piece.tempo = args[0].to_i
       end # don't handle anything else for now
@@ -45,7 +49,7 @@ module Midiparser
       when "End_track"
         track_end(track, time)
       when "Title_t"
-        track_title(track, args.reduce(:+))
+        track_title(track, handle_text(args)) 
       when "Note_on_c"
         note_on(track, time, args[1].to_i, args[2].to_i)
       when "Note_off_c"
@@ -83,6 +87,14 @@ module Midiparser
       else
         puts "Off event before on event. track: #{track}, time: #{time}, pitch: #{note}"
       end
+    end
+    
+    # the line splitting above can mess with text output from midicsv,
+    # if the text contains commas followed by spaces. Also there are 
+    # extraneous quotation marks surrounding the text output that should
+    # be removed.
+    def handle_text(arr)
+      arr.inject { |concatenation, str| concatenation << ", " << str }[1...-1]
     end
   end
 end
